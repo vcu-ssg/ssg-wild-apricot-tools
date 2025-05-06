327d301f4340e56cf789d68997c7e3cd4812ff84
John Leonard <jdleonard@vcu.edu>
2025-05-06 08:47:12 -0400

    Cleaned repo: removed ignored files from version control

--- fantools/_main.py
+++ fantools/_main.py
@@ -5,8 +5,9 @@ import requests
 
 
 @click.group(invoke_without_command=True)
+@click.option('--write-certs', is_flag=True, default=False, help='Write WA and Zscaler root certs on failure.')
 @click.pass_context
-def cli(ctx):
+def cli(ctx,write_certs):
     """FanTools CLI."""
 
     try:
@@ -17,21 +18,22 @@ def cli(ctx):
         click.echo("\n" + api.fix_tls_error_instructions())
 
         # Try to extract and save the TLS certificate
-        try:
-            cert_path = api.extract_tls_cert_to_file()
-            click.secho(f"\nIntercepting certificate saved to: {cert_path}", fg="green")
-            click.secho("You can now add this certificate to your trusted store.", fg="green")
-        except Exception as cert_err:
-            click.secho("Failed to extract and save the TLS certificate automatically.", fg="red")
-            click.secho(str(cert_err), fg="yellow")
+        if write_certs:
+            try:
+                cert_path = api.extract_tls_cert_to_file()
+                click.secho(f"\nIntercepting certificate saved to: {cert_path}", fg="green")
+                click.secho("You can now add this certificate to your trusted store.", fg="green")
+            except Exception as cert_err:
+                click.secho("Failed to extract and save the TLS certificate automatically.", fg="red")
+                click.secho(str(cert_err), fg="yellow")
 
-        try:
-            cert_path = api.extract_zscaler_root_cert()  # replaces extract_tls_cert_to_file()
-            click.secho(f"\nZscaler root certificate extracted and saved to: {cert_path}", fg="green")
-            click.secho("You can now add this certificate to your trusted store.", fg="green")
-        except Exception as cert_err:
-            click.secho("Failed to extract and save the Zscaler root certificate automatically.", fg="red", err=True)
-            click.secho(str(cert_err), fg="yellow", err=True)
+            try:
+                cert_path = api.extract_zscaler_root_cert()  # replaces extract_tls_cert_to_file()
+                click.secho(f"\nZscaler root certificate extracted and saved to: {cert_path}", fg="green")
+                click.secho("You can now add this certificate to your trusted store.", fg="green")
+            except Exception as cert_err:
+                click.secho("Failed to extract and save the Zscaler root certificate automatically.", fg="red", err=True)
+                click.secho(str(cert_err), fg="yellow", err=True)
 
         raise click.Abort()
 
